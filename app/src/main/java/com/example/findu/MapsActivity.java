1c88371d2ba45884085e0acf544c94ce7899fb50
Cheng Xue <74628712+moonpiiiiie@users.noreply.github.com>
2022-06-26 10:30:30 -0700

    fix nav (#7)
    * enable permission check, map type change pending
    
    * fix bottom navigation

--- app/src/main/java/com/example/findu/MapsActivity.java
+++ app/src/main/java/com/example/findu/MapsActivity.java
@@ -1,8 +1,15 @@
 package com.example.findu;
 
+import static android.Manifest.permission.ACCESS_BACKGROUND_LOCATION;
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.content.ContentValues.TAG;
+
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
+
+import android.annotation.SuppressLint;
 
 import android.content.ClipData;
 import android.content.Intent;
@@ -15,7 +22,10 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.tasks.Task;
+
 import com.google.android.libraries.places.api.Places;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -23,7 +33,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.findu.databinding.ActivityMapsBinding;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.PlaceLikelihood;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
+import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+import java.util.Collections;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, AdapterView.OnItemClickListener {
 
@@ -44,11 +63,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+
+        getLocationPermission();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+        // Initialize the SDK
         Places.initialize(getApplicationContext(), BuildConfig.MAPS_API_KEY);
+        // Create a new PlacesClient instance
+        PlacesClient placesClient = Places.createClient(this);
+
+        Places.initialize(getApplicationContext(), BuildConfig.MAPS_API_KEY);
+
         // set bottom navigation connection
         bottomNav = findViewById(R.id.bottom_nav);
         bottomNav.setSelectedItemId(R.id.map_nav_button);
@@ -74,6 +103,35 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, precision);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner_mapType.setAdapter(arrayAdapter);
+
+//        // Use fields to define the data types to return.
+//        List<Place.Field> placeFields = Collections.singletonList(Place.Field.NAME);
+//
+//// Use the builder to create a FindCurrentPlaceRequest.
+//        FindCurrentPlaceRequest request = FindCurrentPlaceRequest.newInstance(placeFields);
+//
+//
+//// Call findCurrentPlace and handle the response (first check that the user has granted permission).
+//        if (ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+//            @SuppressLint("MissingPermission") Task<FindCurrentPlaceResponse> placeResponse = placesClient.findCurrentPlace(request);
+//            placeResponse.addOnCompleteListener(task -> {
+//                if (task.isSuccessful()){
+//                    FindCurrentPlaceResponse response = task.getResult();
+//                    for (PlaceLikelihood placeLikelihood : response.getPlaceLikelihoods()) {
+//                        Log.i(TAG, String.format("Place '%s' has likelihood: %f",
+//                                placeLikelihood.getPlace().getName(),
+//                                placeLikelihood.getLikelihood()));
+//                    }
+//                } else {
+//                    Exception exception = task.getException();
+//                    if (exception instanceof ApiException) {
+//                        ApiException apiException = (ApiException) exception;
+//                        Log.e(TAG, "Place not found: " + apiException.getStatusCode());
+//                    }
+//                }
+//            });
+//        }
+
     }
 
     /**
@@ -95,12 +153,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
         getLocationPermission();
+
     }
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
         String itemSelected = adapterView.getItemAtPosition(i).toString();
         Toast.makeText(this, itemSelected, Toast.LENGTH_SHORT).show();
+
+        if (itemSelected.equals("Normal")) {
+            Log.v("map type select", itemSelected);
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        }
+        else if (itemSelected.equals("Satellite")) {
+            Log.v("map type select", itemSelected);
+            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        }
+        else if (itemSelected.equals("Terrain")) {
+            Log.v("map type select", itemSelected);
 
         if (itemSelected.equals("Normal")) {
             Log.v("maptype select", itemSelected);
@@ -112,6 +182,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
         else if (itemSelected.equals("Terrain")) {
             Log.v("maptype select", itemSelected);
+
             mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
         }
     }
@@ -123,11 +194,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
          * onRequestPermissionsResult.
          */
         if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                ACCESS_FINE_LOCATION)
                 android.Manifest.permission.ACCESS_FINE_LOCATION)
+
                 == PackageManager.PERMISSION_GRANTED) {
             locationPermissionGranted = true;
         } else {
             ActivityCompat.requestPermissions(this,
+                    new String[]{ACCESS_FINE_LOCATION},
                     new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
                     PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
         }
--- app/src/main/java/com/example/findu/ProfileActivity.java
+++ app/src/main/java/com/example/findu/ProfileActivity.java
@@ -15,12 +15,13 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         bottomNav = findViewById(R.id.bottom_nav);
-        bottomNav.setSelectedItemId(R.id.map_nav_button);
+        bottomNav.setSelectedItemId(R.id.profile_nav_button);
         bottomNav.setOnItemSelectedListener(item -> {
             switch (item.getItemId()) {
                 case R.id.map_nav_button:
                     startActivity(new Intent(getApplicationContext(), MapsActivity.class));
                     overridePendingTransition(0,0);
+                    return true;
                 case R.id.post_nav_button:
                     startActivity(new Intent(getApplicationContext(), MainActivity.class));
                     overridePendingTransition(0,0);
