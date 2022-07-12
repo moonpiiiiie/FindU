016e85f2504ce0c7fabad407a36ce09279a9d4ef
Cheng Xue <cheng.xue910@gmail.com>
2022-07-12 14:05:35 -0700

    sign up success toast

--- app/src/main/java/com/example/findu/SignUpActivity.java
+++ app/src/main/java/com/example/findu/SignUpActivity.java
@@ -110,6 +110,7 @@ public class SignUpActivity extends AppCompatActivity {
                 if (!task.isSuccessful()) {
                     Toast.makeText(SignUpActivity.this, "Sign Up Failed", Toast.LENGTH_SHORT).show();
                 } else {
+                    Toast.makeText(SignUpActivity.this, "Sign Up Successful!", Toast.LENGTH_SHORT).show();
                     Log.w("sign up activity", "createUserWithEmail:failure", task.getException());
                     userId = mAuth.getCurrentUser().getUid();
                     DocumentReference documentRef = db.collection("users").document(userId);
